45f1f4a27cb309f880cd0a0e462227ad68d7afc5
Alex Shvarev <alex@shvarev.com>
2023-11-18 02:54:44 +0300

    v0.3

--- test.py
+++ test.py
@@ -10,6 +10,8 @@ from torch.utils.data import DataLoader, TensorDataset
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.model_selection import train_test_split
 
+from tqdm import tqdm
+
 
 class DataProcessor:
     def __init__(self, file_name: str = "datasets/transactions.csv"):
@@ -22,7 +24,7 @@ class DataProcessor:
         for group in df.groups:
             d: pd.DataFrame = df.get_group(group)
 
-            d = d.drop(labels=["client_id", 'trans_time', 'trans_city', 'term_id'], axis=1)
+            d = d.drop(labels=['trans_time', 'trans_city', 'term_id'], axis=1)
             dataset.append(d.values.tolist())
 
         self.df = dataset
@@ -40,30 +42,12 @@ class DataProcessor:
     def get_df(file_name: str = "datasets/transactions.csv") -> pd.DataFrame:
         return pd.read_csv(file_name)
 
-    @staticmethod
-    def preprocess_df(df: pd.DataFrame) -> pd.DataFrame:
-        df = df.drop(labels=["RowNumber", "CustomerId", "Surname"], axis=1)
-
-        ohe = OneHotEncoder()
-        tf = ohe.fit_transform(df[["Geography"]])
-        df[ohe.categories_[0]] = tf.toarray()
-
-        ohe = OneHotEncoder(drop=["Female"])
-        tf = ohe.fit_transform(df[["Gender"]])
-        df["Male"] = tf.toarray()
-
-        return df.drop(labels=["Geography", "Gender"], axis=1)
-
-    def XY_split(self):
-        return self.df.drop(labels=["Exited"], axis=1), self.df["Exited"]
-
 
 class GRUNetwork(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super(GRUNetwork, self).__init__()
         self.gru = nn.GRU(input_size, hidden_size, batch_first=True)
         self.fc = nn.Linear(hidden_size, output_size)
-
 
     def forward(self, x):
         # x shape: (batch_size, sequence_length, input_size)
@@ -78,10 +62,70 @@ class GRUNetwork(nn.Module):
 
         return F.sigmoid(output)
 
-    def init_hidden(self, batch_size):
-        weight = next(self.parameters()).data
-        hidden = weight.new(self.n_layers, batch_size, self.hidden_dim).zero_()
-        return hidden
+
+def get_train():
+    return pd.read_csv("datasets/train.csv")
+
+
+def train_model(nn: GRUNetwork, criterion, optimizer, train, dp):
+    for epoch in range(2):
+        print(f"Epoch {epoch}")
+
+        nn.train()
+        loss = 0
+
+        answers = 0
+        allans = 0
+
+        for X in tqdm(dp.df):
+            clid = X[0][0]
+
+            if not train[train["client_id"] == clid].empty:
+                y = torch.Tensor([int(train[train["client_id"] == clid]["gender"])]).view(1, 1)
+            else:
+                continue
+
+            for i in range(len(X)):
+                X[i] = X[i][1:]
+            X = torch.Tensor([X])
+
+            # Forward
+            y_pred = nn(X)
+
+            # Loss computation
+            loss = criterion(y_pred, y)
+
+            # Backward
+            loss.backward()
+
+            # Update weights
+            optimizer.step()
+
+            # Zero gradients
+            optimizer.zero_grad()
+
+            # log
+            acc = (y_pred.round() == y).float()  # right answer
+            answers += acc
+            allans += 1
+
+        print(answers / allans)
+
+        # Debug
+        # self.nn.eval()
+        #
+        # self.train_acc_history.append(self.test_model())
+        # self.test_acc_history.append(self.eval_model())
+        # self.loss_history.append(loss.item())
+
+        # Printing provisional results
+        # if epoch % 10 == 9 or epoch == 0:
+        #     print(f"Epoch {epoch + 1}: Loss {loss.item():.4f}: Accuracy {self.test_acc_history[-1]}")
+
+    print("Finished Training")
+
+
+train = get_train()
 
 
 # Example usage:
@@ -91,12 +135,18 @@ output_size = 1
 
 model = GRUNetwork(input_size, hidden_size, output_size)
 
-# Assuming you have input data x with shape (batch_size, sequence_length, input_size)
+criterion = nn.BCELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
 dp = DataProcessor()
+
+train_model(model, criterion, optimizer, train, dp=dp)
+exit(0)
+
+# Assuming you have input data x with shape (batch_size, sequence_length, input_size)
 print(dp.df[0])
 
-print()
-x = torch.Tensor([dp.df[0]])  # Example input data
+x = torch.Tensor([dp.df[1]])  # Example input data
 print(x.shape, "shape")
 
 output = model(x)
