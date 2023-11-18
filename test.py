3911d592a4d595ce14f6b293b36cde7a21d05587
alex <alex@shvarev.com>
2023-11-18 11:12:08 +0300

    v0.4 mvp

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data import DataLoader, TensorDataset
 
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
@@ -14,29 +15,46 @@ from tqdm import tqdm
 
 
 class DataProcessor:
-    def __init__(self, file_name: str = "datasets/transactions.csv"):
+    def __init__(self, file_name: str = "datasets/transactions.csv", valid: str = "datasets/train.csv", predict_from: int = 1000):
+        self.valid = valid
+        self.predict_from = predict_from
         df = self.get_df(file_name)
-        print(df["client_id"].nunique())
+
+        self.X_train, self.y_train = self.get_train_dataset(df)
+
+
+    def get_train(self):
+        return pd.read_csv(self.valid)
+
+    def get_train_dataset(self, df):
+        print(f'Unique clients: {df["client_id"].nunique()}')
         df = df.groupby("client_id")
 
-        dataset = []
+        X_train = []
+        y_train = []
+
+        train = self.get_train()
 
         for group in df.groups:
             d: pd.DataFrame = df.get_group(group)
+            d = d[:self.predict_from]
 
-            d = d.drop(labels=['trans_time', 'trans_city', 'term_id'], axis=1)
-            dataset.append(d.values.tolist())
+            if not train[train["client_id"] == group].empty:
+                y = train[train["client_id"] == group]["gender"].iloc[0]
+                y_train.append(y)
+            else:
+                continue
 
-        self.df = dataset
+            d = d.drop(labels=['client_id', 'trans_time', 'trans_city', 'term_id'], axis=1)
+            X_train.append(torch.Tensor(d.values.tolist()))
 
+        X_train = pad_sequence(X_train, batch_first=True)
+        y_train = torch.Tensor(y_train).view(-1, 1)
 
-        # dataset = np.array(dataset, dtype="object")
-        # print(dataset.shape)
+        print(f"X_Train_shape: {X_train.shape}")
+        print(f"X_Train_shape: {y_train.shape}")
 
-        # train = torch.LongTensor(dataset)
-        # print(train.shape())
-        # self.df = self.preprocess_df(self.get_df(file_name))
-        # self.X, self.y = self.XY_split()
+        return X_train, y_train
 
     @staticmethod
     def get_df(file_name: str = "datasets/transactions.csv") -> pd.DataFrame:
@@ -46,108 +64,194 @@ class DataProcessor:
 class GRUNetwork(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super(GRUNetwork, self).__init__()
-        self.gru = nn.GRU(input_size, hidden_size, batch_first=True)
+        self.gru = nn.LSTM(input_size, hidden_size, num_layers=2, batch_first=True)
         self.fc = nn.Linear(hidden_size, output_size)
+
+        self.linear_layer = nn.Sequential(
+            nn.Linear(hidden_size, 16),
+            nn.LeakyReLU(0.01),
+            nn.BatchNorm1d(16),
+            nn.Dropout(p=0.25),
+
+            nn.Linear(16, 8),
+            nn.LeakyReLU(0.01),
+            nn.Dropout(p=0.1),
+
+            nn.Linear(8, 1)
+        )
 
     def forward(self, x):
         # x shape: (batch_size, sequence_length, input_size)
-        gru_out, _ = self.gru(x)
+        rnn_out, _ = self.gru(x)
         # gru_out shape: (batch_size, sequence_length, hidden_size)
 
         # Taking the output from the last time step
-        last_output = gru_out[:, -1, :]
+        last_output = rnn_out[:, -1, :]
 
-        output = self.fc(last_output)
+        output = self.linear_layer(last_output)
         # output shape: (batch_size, output_size)
 
         return F.sigmoid(output)
 
 
-def get_train():
-    return pd.read_csv("datasets/train.csv")
+class Worker:
+    def __init__(self, settings: dict):
+        self.settings = settings
+
+        self.dataprocessor = DataProcessor(file_name=settings["transactions"], valid=settings["train"])
+
+        self.dataset = TensorDataset(self.dataprocessor.X_train, self.dataprocessor.y_train)
+
+        # Example usage:
+        input_size = 3
+        hidden_size = 28
+        output_size = 1
+
+        self.nn = GRUNetwork(input_size, hidden_size, output_size)
+
+        self.train_acc_history = []
+        self.test_acc_history = []
+        self.loss_history = []
+
+    def train_model(self):
+        print("\nStarting Training")
+
+        criterion = nn.BCELoss()
+        optimizer = torch.optim.Adam(self.nn.parameters(), lr=self.settings["learning_rate"])
+
+        loader = DataLoader(self.dataset, batch_size=self.settings["batch_size"], shuffle=True)
+
+        # Training loop
+        for epoch in range(self.settings["num_epochs"]):
+            print(f"Epoch {epoch+1}")
+
+            for X, y in tqdm(loader):
+                # Forward
+                y_pred = self.nn(X)
+
+                # Loss computation
+                loss = criterion(y_pred, y)
+
+                # Backward
+                loss.backward()
+
+                # Update weights
+                optimizer.step()
+
+                # Zero gradients
+                optimizer.zero_grad()
+
+                # LOG
+                acc = (y_pred.round() == y).float().mean()
+
+                self.train_acc_history.append(acc)
+                self.loss_history.append(loss.item())
+
+    def show_stats(self):
+        plt.plot(self.train_acc_history, label="Train")
+        # plt.plot(self.test_acc_history, label="Test")
+
+        plt.legend()
+        plt.ylabel("Accuracy")
+        plt.xlabel("Batch")
+        plt.grid()
+
+        # if savefig:
+        #     plt.savefig(savefig)
+        # else:
+        plt.show()
+
+# def train_model(nn: GRUNetwork, criterion, optimizer, train, dp):
+#     for epoch in range(2):
+#         print(f"Epoch {epoch}")
+#
+#         nn.train()
+#         loss = 0
+#
+#         answers = 0
+#         allans = 0
+#
+#         for X in tqdm(dp.df):
+#             clid = X[0][0]
+#
+#             if not train[train["client_id"] == clid].empty:
+#                 y = torch.Tensor([int(train[train["client_id"] == clid]["gender"])]).view(1, 1)
+#             else:
+#                 continue
+#
+#             for i in range(len(X)):
+#                 X[i] = X[i][1:]
+#             X = torch.Tensor([X])
+#
+#             # Forward
+#             y_pred = nn(X)
+#
+#             # Loss computation
+#             loss = criterion(y_pred, y)
+#
+#             # Backward
+#             loss.backward()
+#
+#             # Update weights
+#             optimizer.step()
+#
+#             # Zero gradients
+#             optimizer.zero_grad()
+#
+#             # log
+#             acc = (y_pred.round() == y).float()  # right answer
+#             answers += acc
+#             allans += 1
+#
+#         print(answers / allans)
+#
+#         # Debug
+#         # self.nn.eval()
+#         #
+#         # self.train_acc_history.append(self.test_model())
+#         # self.test_acc_history.append(self.eval_model())
+#         # self.loss_history.append(loss.item())
+#
+#         # Printing provisional results
+#         # if epoch % 10 == 9 or epoch == 0:
+#         #     print(f"Epoch {epoch + 1}: Loss {loss.item():.4f}: Accuracy {self.test_acc_history[-1]}")
+#
+#     print("Finished Training")
 
 
-def train_model(nn: GRUNetwork, criterion, optimizer, train, dp):
-    for epoch in range(2):
-        print(f"Epoch {epoch}")
+if __name__ == "__main__":
+    settings = {
+        "transactions": "datasets/transactions.csv",
+        "train": "datasets/train.csv",
+        "batch_size": 189,
+        "learning_rate": 0.001,
+        "num_epochs": 5
+    }
 
-        nn.train()
-        loss = 0
-
-        answers = 0
-        allans = 0
-
-        for X in tqdm(dp.df):
-            clid = X[0][0]
-
-            if not train[train["client_id"] == clid].empty:
-                y = torch.Tensor([int(train[train["client_id"] == clid]["gender"])]).view(1, 1)
-            else:
-                continue
-
-            for i in range(len(X)):
-                X[i] = X[i][1:]
-            X = torch.Tensor([X])
-
-            # Forward
-            y_pred = nn(X)
-
-            # Loss computation
-            loss = criterion(y_pred, y)
-
-            # Backward
-            loss.backward()
-
-            # Update weights
-            optimizer.step()
-
-            # Zero gradients
-            optimizer.zero_grad()
-
-            # log
-            acc = (y_pred.round() == y).float()  # right answer
-            answers += acc
-            allans += 1
-
-        print(answers / allans)
-
-        # Debug
-        # self.nn.eval()
-        #
-        # self.train_acc_history.append(self.test_model())
-        # self.test_acc_history.append(self.eval_model())
-        # self.loss_history.append(loss.item())
-
-        # Printing provisional results
-        # if epoch % 10 == 9 or epoch == 0:
-        #     print(f"Epoch {epoch + 1}: Loss {loss.item():.4f}: Accuracy {self.test_acc_history[-1]}")
-
-    print("Finished Training")
-
-
-train = get_train()
-
+    worker = Worker(settings=settings)
+    worker.train_model()
+    worker.show_stats()
 
 # Example usage:
-input_size = 3
-hidden_size = 10
-output_size = 1
-
-model = GRUNetwork(input_size, hidden_size, output_size)
-
-criterion = nn.BCELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-
-dp = DataProcessor()
-
-train_model(model, criterion, optimizer, train, dp=dp)
-exit(0)
-
-# Assuming you have input data x with shape (batch_size, sequence_length, input_size)
-print(dp.df[0])
-
-x = torch.Tensor([dp.df[1]])  # Example input data
-print(x.shape, "shape")
-
-output = model(x)
-print(output.shape)
+# input_size = 3
+# hidden_size = 10
+# output_size = 1
+#
+# model = GRUNetwork(input_size, hidden_size, output_size)
+#
+# criterion = nn.BCELoss()
+# optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+#
+# dp = DataProcessor()
+#
+# train_model(model, criterion, optimizer, train, dp=dp)
+# exit(0)
+#
+# # Assuming you have input data x with shape (batch_size, sequence_length, input_size)
+# print(dp.df[0])
+#
+# x = torch.Tensor([dp.df[1]])  # Example input data
+# print(x.shape, "shape")
+#
+# output = model(x)
+# print(output.shape)
